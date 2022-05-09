49e87a914a59d66d3471f0738d0c7806069773c0
tluczekk <tluczekk@o2.pl>
2022-05-09 14:03:42 +0200

    Distance correction

--- src/util/handwriting_distance.py
+++ src/util/handwriting_distance.py
@@ -1,6 +1,7 @@
 import fastdtw
 from handwriting_features import get_features
+from scipy.spatial.distance import euclidean
 
 def get_distance(im1, im2):
-    distance, path = fastdtw(get_features(im1), get_features(im2))
+    distance, path = fastdtw(get_features(im1), get_features(im2), distance=euclidean)
     return distance
